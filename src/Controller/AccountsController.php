67fbe50a19ddf9b4022c5c8af8fc8dc317144558
Maxime Cazade <maxime.cazade@orange.fr>
2017-03-24 16:48:02 +0100

    Modifs de Vincent : objectco.ctp dans Accounts

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -205,4 +205,33 @@ class AccountsController extends AppController
         $match = $this->Workouts->find('all');
         $this->set('Workouts', $match);                 
     }
+
+    function objetsco()
+    {
+        $this->loadModel("Devices");
+
+      if ($this->request->is("post")){
+        if(isset($_POST['ajouter'])){
+          $member_id=$this->request->data["member_id"];
+          $serial=$this->request->data["serial"];
+          $description=$this->request->data["description"];
+          $this->Devices->addobjets($member_id, $serial, $description);
+        }
+      }
+      $w=$this->Devices->find()->where(["member_id"=>$this->request->Session()->read('Auth.User.id')]);
+        $this->Set("ws",$w->toArray());
+    }
+    public function delete($id)
+    {
+        $this->loadModel("Devices");
+        $this->request->allowMethod(['post', 'delete']);
+        $device = $this->Devices->get($id);
+        if ($this->Devices->delete($device)) {
+            $this->Flash->success(__('The device has been deleted.'));
+        } else {
+            $this->Flash->error(__('The device could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'objetsco']);
+    }
 }
